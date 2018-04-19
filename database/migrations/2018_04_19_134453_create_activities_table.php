5da5d1efb65caa09d55b00b230d0c3d6bbb8abdd
unknown <thanhnamdk2710@gmail.com>
2018-04-19 14:17:26 +0700

    Create Migration

---
+++ database/migrations/2018_04_19_134453_create_activities_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateActivitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('activities', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('activitable_id');
+            $table->string('activitable_type');
+            $table->string('action');
+            $table->integer('user_id');
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('activities');
+    }
+}
